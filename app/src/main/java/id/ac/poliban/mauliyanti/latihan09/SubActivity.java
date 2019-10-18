6375b9478c96d42158d0ee731b72a2bbea3cb544
mauliyanti <normauliyanti@gmail.com>
2019-10-19 07:26:09 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mauliyanti/latihan09/SubActivity.java
@@ -0,0 +1,33 @@
+package id.ac.poliban.mauliyanti.latihan09;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SubActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        //create object for views
+        EditText etYourName = findViewById(R.id.etName);
+        Button btFinish = findViewById(R.id.btFinish);
+
+        //event handler
+        btFinish.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            String data = etYourName.getText(). toString();
+            intent.putExtra("subactivity", data);
+            setResult(Activity.RESULT_OK, intent);
+            finish(); //destroy subactivity
+
+        });
+    }
+}
